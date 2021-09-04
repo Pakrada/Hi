a4c2c39ce7212c8e8d722f3f96be91daf2b2d0e2
Pakrada <s6302041510079@email.kmutnb.ac.th>
2021-09-04 12:14:57 +0700

    ืnew

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mobileappweek1/test.dart';
+
 void main(){
-  print("hello");
+  runApp(Pak());
+}
+
+class Pak extends StatelessWidget {
+  const Pak({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Test(),
+    );
+  } /// test git
 }
